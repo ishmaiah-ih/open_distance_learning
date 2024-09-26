87d28565ff2301d3f990ad093e0bd91cd1b1281d
ishmaiah-ih <mwalwandaishmael@gmail.com>
2024-09-27 01:05:28 +0200

    navbar corrected

--- resources/views/front/includes/navbar.blade.php
+++ resources/views/front/includes/navbar.blade.php
@@ -1,74 +1,64 @@
 <nav class="navbar navbar-expand-lg navbar-color">
     <div class="container p-3">
-        <span><img src="assets/img/mubas-logo.png" class="w-25 h-25 rounded-circle"></span>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        <span class="d-block mx-auto mx-lg-0">
+            <img src="assets/img/mubas-logo.png" class="w-25 h-25 rounded-circle">
+        </span>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon "></span>
+            <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto ">
+        <div class="collapse navbar-collapse justify-content-center justify-content-lg-end" id="navbarNav">
+            <ul class="navbar-nav text-center text-lg-end">
                 <li class="nav-item">
-                    <a class="nav-link "
-                       aria-current="page" href="{{route('user.dashboard')}}">Home</a>
+                    <a class="nav-link" aria-current="page" href="{{ route('user.dashboard') }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link "
-                       href="{{route('profile.edit')}}">Profile</a>
+                    <a class="nav-link" href="{{ route('profile.edit') }}">Profile</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link">
-                        {{Auth::User()->name}}
-                    </a>
+                    <a class="nav-link">{{ Auth::User()->name }}</a>
                 </li>
-                <!--                <li class="nav-item">-->
-                <!--                    <span id="current-time" class="navbar-text font-weight-bold mr-2"></span>-->
-                <!--                </li>-->
                 <li class="nav-item">
-                    <a class="nav-link "
-                       href="#">
-
-                        <!--                        <i class="fa fa-bell-o" aria-hidden="true"></i>-->
+                    <a class="nav-link" href="#">
                         <i class="fa-regular fa-bell"></i>
-
                     </a>
                 </li>
                 <li class="nav-item">
-
                     <form action="{{ route('logout') }}" method="POST" class="inline">
                         @csrf
-{{--                        <span class="font-weight-bold">--}}
-{{--                                <i class="fa fa-power-off text-white" aria-hidden="true"></i>--}}
-{{--                            </span>--}}
-                                                <button type="submit" class="block text-white font-bold hover:bg-blue-700 px-3 py-2 rounded"><i class="fa fa-power-off"></i></button>
+                        <button type="submit" class="btn btn-link text-white font-bold px-3 py-2">
+                            <i class="fa fa-power-off"></i>
+                        </button>
                     </form>
-                    {{--                    <a href="{{route('logout')}}" class="nav-link">--}}
-                    {{--                            <span class="font-weight-bold">--}}
-                    {{--                                <i class="fa fa-power-off text-white" aria-hidden="true"></i>--}}
-                    {{--                            </span>--}}
-                    {{--                    </a>--}}
                 </li>
             </ul>
         </div>
     </div>
 </nav>
 
-<style>
-    .nav-link.active {
-        color: #dc3545 !important;
-        /* Bootstrap danger color */
-        font-weight: bold;
-    }
+<!-- Bootstrap JS and dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 
+<style>
     .navbar-color {
         background-color: #074173;
     }
 
-    .nav-item:hover .nav-link {
-        color: navajowhite;
+    .navbar-toggler {
+        border: none;
     }
 
-    .navbar-text {
-        color: yellow;
-        margin-left: 20px;
+    .navbar-toggler-icon {
+        background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23fff' viewBox='0 0 30 30'%3E%3Cpath stroke='rgba(255, 255, 255, 0.5)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+    }
+
+    .nav-link.active {
+        color: #dc3545 !important;
+        font-weight: bold;
+    }
+
+    .nav-item:hover .nav-link {
+        color: navajowhite !important;
     }
 </style>
